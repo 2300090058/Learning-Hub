3126c6532a881c6287571360890701b10f7fe5e6
2300090058 <163714386+2300090058@users.noreply.github.com>
2025-05-11 10:21:17 +0530

    Add files via upload

---
+++ LearningHub/src/main/java/com/klu/LearningHub/service/StudService.java
@@ -0,0 +1,29 @@
+package com.klu.LearningHub.service;
+
+import com.klu.LearningHub.model.Student;
+import com.klu.LearningHub.rep.StudRep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class StudService {
+    @Autowired
+    private StudRep repo;
+
+    public void saveStudent(Student s) {
+        s.setSid(generateStudentId());
+        repo.save(s);
+    }
+
+    public Optional<Student> validateStudent(String email, String password) {
+        return repo.findByEmailAndPassword(email, password);
+    }
+
+    private String generateStudentId() {
+        Integer maxId = repo.findMaxStudentIdNumber();
+        int nextId = (maxId != null) ? maxId + 1 : 101;
+        return "sid" + nextId;
+    }
+}
